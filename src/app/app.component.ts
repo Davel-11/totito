a23109af08bef7016003944cc77bc0107edb60c0
davel <davelaltan@gmail.com>
2019-03-19 21:00:25 -0600

    xx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Movement } from './movement.model';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-root',
@@ -14,78 +17,170 @@ export class AppComponent implements OnInit {
   totito: number[][];
   valueUser = 'X';
 
+  learnMachine = [];
+
   ngOnInit() {
 
     this.totito = [];
     this.initializeBoard();
 
 
-    this.firstHumanMove();
-
-    this.totito[0][0] = 2;
-    this.totito[1][1] = 2;
-    this.totito[2][2] = 2;
   }
+
+  
 
   sendCoordinates(x: number, y: number, jugador: string) {
 
+    
     this.totito[x][y] = 1;
+    if( this.winner(this.totito) ) {
+        console.log("winner jugarodr" )
+    } else {
+      // this.tableOne(this.totito);
+      this.moverMaquina();
+      if( this.winner(this.totito) ) {
+        console.log("winner maquina" )
+      }
+    } 
+
     console.log('totito hasta el momento', this.totito);
 
-    this.tableOne(this.totito);
+    
 
   }
 
-  initializeBoard() {
-    for (let i = 0; i < 3; i++) {
-      this.totito[i] = [];
-      for (let j = 0; j < 3; j++) {
+ 
+  winner(totito) : boolean {
+  
 
-        this.totito[i][j] = 0;
-
+	// ESQUINA SUPERIOR IZQUIERDA
+  let movi : Movement[] = [];
+  let position = [];
+  
+  if (totito[0][0] != 0) {
+    // ABAJO
+    if (totito[1][0] != 0 && totito[1][0] == totito[0][0]) {
+      if (totito[2][0] != 0 && totito[2][0] == totito[1][0]) {
+        movi.push( new Movement(0, 0) );
+        movi.push(new Movement(1, 0));
+        movi.push(new Movement(2, 0));
+        this.learnMachine.push(movi);
+        return true;
+      }
+    }
+    // AL LADO
+    if (totito[0][1] != 0 && totito[0][1] == totito[0][0]) {
+      if (totito[0][2] != 0 && totito[0][2] == totito[0][1]) {
+        movi.push(new Movement(0, 0));
+        movi.push(new Movement(0, 1));
+        movi.push(new Movement(0, 2));
+        this.learnMachine.push(movi);
+        return true;
+      }
+    }
+    // DIAGONAL
+    if (totito[1][1] != 0 && totito[1][1] == totito[0][0]) {
+      if (totito[2][2] != 0 && totito[2][2] == totito[1][1]) {
+        movi.push(new Movement(0, 0));
+        movi.push(new Movement(1, 1));
+        movi.push(new Movement(2, 2));
+        this.learnMachine.push(movi);
+        return true;
       }
     }
   }
+  // MEDIO
+  if (totito[0][1] != 0) {
+    if (totito[1][1] != 0 && totito[1][1] == totito[0][1]) {
+      if (totito[2][1] != 0 && totito[2][1] == totito[1][1]) {
+        movi.push(new Movement(0, 1));
+        movi.push(new Movement(1, 1));
+        movi.push(new Movement(2, 1));
+        this.learnMachine.push(movi);
+        return true;
+      }
+    }
+  }
+  if (totito[1][0] != 0) {
+    // ABAJO
+    if (totito[1][1] != 0 && totito[1][1] == totito[1][0]) {
+      if (totito[1][2] != 0 && totito[1][2] == totito[1][1]) {
+        movi.push(new Movement(1, 0));
+        movi.push(new Movement(1, 1));
+        movi.push(new Movement(1, 2));
+        this.learnMachine.push(movi);
+        return true;
+      }
+    }
+  }
+  // ESQUINA SUPERIOR DERECHA
+  if (totito[0][2] != 0) {
+    // ABAJO
+    if (totito[1][2] != 0 && totito[1][2] == totito[0][2]) {
+      if (totito[2][2] != 0 && totito[2][2] == totito[1][2]) {
+        movi.push(new Movement(0, 2));
+        movi.push(new Movement(1, 2));
+        movi.push(new Movement(2, 2));
+        this.learnMachine.push(movi);
+        return true;
+      }
+    }
+    // DIAGONAL
+    if (totito[1][1] != 0 && totito[1][1] == totito[0][2]) {
+      if (totito[2][0] != 0 && totito[2][0] == totito[1][1]) {
+        movi.push(new Movement(1, 1));
+        movi.push(new Movement(0, 2));
+        movi.push(new Movement(2, 0));
+        this.learnMachine.push(movi);
+        return true;
+      }
+    }
+  }
+  // ESQUINA INFERIOR DERECHA
+  if (totito[2][0] != 0) {
+    // ABAJO
+    if (totito[2][1] != 0 && totito[2][1] == totito[2][0]) {
+      if (totito[2][2] != 0 && totito[2][2] == totito[2][1]) {
+        movi.push(new Movement(2, 0));
+        movi.push(new Movement(2, 1));
+        movi.push(new Movement(2, 2));
+        this.learnMachine.push(movi);
+        return true;
+      }
+    }
+  }
+  return false;
+
+  }
+
 
   tableOne(totito: number[][]) {
 
-
     let position = [];
-    let positionMove = [];
-    let MovementIs = 0;
 
+    let count = 0;
+   
 
-    for (let i = 0; i < 9; i++) {
+    while ( true ) {
       let randomNumber = Math.floor(Math.random() * 9) + 1;
       position = this.returnPosition(randomNumber);
+      let valor = totito[position[0]][position[1]];
+      count++;
+      if ( valor === 0 ) {
+        totito[position[0]][position[1]] = 2;
+        return;
+      } 
 
-      if ((totito[position[0]][position[1]] != 1)) {
-
-        MovementIs = randomNumber;
-        break;
-
+      if (count ===8 ){
+        return;
       }
+
     }
-
-    positionMove = this.returnPosition(MovementIs);
-    console.log('primer movimiento es; ', positionMove);
-
-
+    
+    
   }
-
-  firstHumanMove() {
-
-    this.sendCoordinates(1, 1, 'human');
-
-
-  }
-
-<<<<<<< HEAD
-  returnPosition(randomNumber: number){
-=======
 
   returnPosition(randomNumber: number) {
->>>>>>> a7a78ea919c7bc4b075fff5f7ef15c7fe06e6660
 
     let position = [];
 
@@ -93,27 +188,79 @@ export class AppComponent implements OnInit {
       position.push(0, 0);
     } else if (randomNumber === 2) {
       position.push(0, 1);
+
     } else if (randomNumber === 3) {
       position.push(0, 2);
+
     } else if (randomNumber === 4) {
       position.push(1, 0);
+
     } else if (randomNumber === 5) {
       position.push(1, 1);
+
     } else if (randomNumber === 6) {
       position.push(1, 2);
+
     } else if (randomNumber === 7) {
       position.push(2, 0);
+
     } else if (randomNumber === 8) {
       position.push(2, 1);
+
     } else if (randomNumber === 9) {
       position.push(2, 2);
-    } else {
-      position.push(99);
-    }
+
+    } 
 
 
     return position;
 
   }
+
+  initializeBoard() {
+    for (let i = 0; i < 3; i++) {
+      this.totito[i] = [];
+      for (let j = 0; j < 3; j++) {
+        this.totito[i][j] = 0;
+      }
+    }
+  }
+
+  moverMaquina() {
+    let maquina = false;
+		if(  this.learnMachine.length > 0 ) {
+			let greatestValue = 0;
+			let greatestMov = [];
+			for (let movi of this.learnMachine) {
+				let pieza = 0;
+				let suma = 0;
+				for (let mo of movi) {
+					pieza = this.totito[mo.x][mo.y];
+					if (pieza != 0 && pieza == 1) {
+						suma++;
+					}
+				}
+				if (suma >= greatestValue) {
+					greatestValue = suma;
+					greatestMov = movi;
+				}
+
+			}
+			for (let mo of greatestMov) {
+				let pieza = this.totito[mo.x][mo.y];
+				if (pieza == 0) {
+          this.totito[mo.x][mo.y] = 2;
+          maquina = true;
+					return;
+				}
+			}
+    } 
+    if(!maquina) {
+
+				this.tableOne(this.totito);
+		}
+	}
+
+
 
 }
---
+++ src/app/movement.model.ts
@@ -0,0 +1,9 @@
+export class Movement 
+{
+    constructor (
+
+        public x : number,
+        public y : number
+
+    ){}
+}
